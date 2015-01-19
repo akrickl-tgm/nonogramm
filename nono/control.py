00bba23a8c4c2e01b25f30d499dca52c38666dfe
Astrid Krickl <akrickl@student.tgm.ac.at>
2015-01-19 12:20:07 +0100

    protocoll updatet

--- nono/control.py
+++ nono/control.py
@@ -7,7 +7,19 @@ class Control():
 
     """
     def getpic(self, breite):
+        """
+        man nimmt eine spaltenbreite (zeile = spalte)
+        dann erstellt man ein array mit der größe breite²
+        dies befüllt man mit random zahlen zwischen 0 und 1
+
+        immer 'breitenanzahl' sind eine zeile
+        und jedes 5te gehört zu der jeweiligen spalte
+        dann zählt man die 1er und speichert sie in die jeweilige reihe bzw spalte
+        und speichert die in ein 2d array (?)
+        :param breite:
+        :return:
+        """
         pass
 
-    def getlosung(self):
+    def getLosung(self):
         return self.losung
