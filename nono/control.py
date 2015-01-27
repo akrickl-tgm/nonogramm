a0d3fc0ac3785685817a450876b2b8251c287065
Astrid Krickl <akrickl@student.tgm.ac.at>
2015-01-27 11:04:14 +0100

    idea of model added

--- nono/control.py
+++ nono/control.py
@@ -1,6 +1,19 @@
 __author__ = 'Astrid Krickl'
 from model.Model import *
 
+
+
+"""
+alle buttons haben 3 zustände
+-1 unbestimmt (lila?)
+0 weiss
+1 schwarz
+
+wenn man auf ein feld drauf klickt kommt man von lila zu weiss und dann zu schwarz und dann wieder zu lila ....
+textfeld felder offen zeigt anzahl der lila felder an + falsch angekreuzte (wird nach jdm klick gechekt)
+neustart alles löschen neues puzzle genereren
+lösen alle felder fertig einfärben 
+"""
 class Model():
     def lala(self):
         pass
